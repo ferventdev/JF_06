48a821ea3fa0705aaf906e0e20a9745e080d4f8c
Aleksandr Shevkunenko <alex.4everyours@gmail.com>
2017-07-19 15:08:53 +0300

    initial commit

---
+++ src/main/java/t02/PropertiesReader.java
@@ -0,0 +1,13 @@
+package t02;
+
+import java.util.Map;
+
+public class PropertiesReader {
+    Map<String, String> properties;
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+
+}
